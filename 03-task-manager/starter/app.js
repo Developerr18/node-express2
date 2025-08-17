9def0f0f31ff7e1de9498aef85596b42901fe24c
Developerr18 <dineshnikum1@gmail.com>
2025-08-17 22:22:00 +0530

    Initial code of Task manager app backend API

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,20 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+
+const tasksRoutes = require("./routes/tasks");
+
+// parse json
+app.use(express.json());
+
+app.get("/hello", (req, res) => {
+  res.send("Task Manager App!");
+});
+
+// use tasks routes
+app.use("/api/v1/tasks", tasksRoutes);
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}...`);
+});
