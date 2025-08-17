16a66fb8328513c40ad5565a2df33d7cf1f12f9c
Developerr18 <dineshnikum1@gmail.com>
2025-08-18 05:02:13 +0530

    install mongoose

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
-
+const connectDB = require("./db/connect");
 const tasksRoutes = require("./routes/tasks");
+require("dotenv").config();
 
 // parse json
 app.use(express.json());
@@ -15,6 +16,13 @@ app.use("/api/v1/tasks", tasksRoutes);
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on PORT ${PORT}...`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, console.log(`Server is running on PORT ${PORT}...`));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
